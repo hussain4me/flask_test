fe1bc99ae8a6d1770d184c8536295491b1c2b58a
hussain4me <hussain4me2008@gmail.com>
2022-03-16 04:44:37 +0100

    Further Update

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "hello World"
+def show_projects():
+    return render_template("index.html")
+
+# @app.route('/')
+# def index():
+#     return "hello World"
  
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="127.0.0.1", port=3000)
